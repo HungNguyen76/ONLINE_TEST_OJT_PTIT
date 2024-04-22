8fdd68367a206e33e1ca928f09b3565a95198df2
huynhbaoduy0210 <149989841+huynhbaoduy0210@users.noreply.github.com>
2024-04-22 08:41:11 +0700

    testanh-duy

--- assets/user/js/tinhdiem.js
+++ assets/user/js/tinhdiem.js
@@ -1,16 +1,3 @@
-function naming() {
-    var userInput = document.getElementById("pname").innerHTML = prompt("Please enter your name!");
-    if (userInput === "") {
-        alert("Please enter your name!");
-        document.getElementById("pname").innerHTML = prompt("Your Name -_-");
-    }
-    var age = document.getElementById("page").innerHTML = parseInt(prompt("your age!"));
-    var place = document.getElementById("pplace").innerHTML = prompt("Your location!");
-    if (place === "") {
-        document.getElementById("pplace").innerHTML = "Unknown";
-    }
-}
-
 document.getElementById('quest-f').addEventListener('submit', estimateFormResult);
 
 function estimateFormResult(event) {
@@ -32,7 +19,7 @@ function estimateFormResult(event) {
     document.getElementById("place").style = "transform: translateX(0%); transition-delay: 0.4s";
     document.getElementById("cong").style = "transform: scale(1,1); transition-delay: 0.2s";
     document.getElementById("degr").style = "transform: scale(1,1); transition-delay: 0.4s";
-    document.getElementById("lineone").style = "transform: translateX(0%); transition-delay: .1s;";
+    // document.getElementById("lineone").style = "transform: translateX(0%); transition-delay: .1s;";
     document.getElementById("linetwo").style = "transform: translateX(0%); transition-delay: .3s;";
     document.getElementById("linethree").style = "transform: translateX(0%); transition-delay: .4s;";
     document.getElementById("crid").style = "animation: aut 2s infinite alternate;";
